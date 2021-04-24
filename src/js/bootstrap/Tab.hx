845716998569e768f26a46f6413a20baf0382f66
Cédric Belin <cedric@belin.io>
2021-04-24 18:23:42 +0200

    Added externs for tab component

---
+++ src/js/bootstrap/Tab.hx
@@ -0,0 +1,21 @@
+package js.bootstrap;
+
+import haxe.extern.EitherType;
+import js.html.Element;
+
+/** Represents a tab. **/
+@:native("bootstrap.Tab")
+extern class Tab {
+
+	/** Creates a new tab. **/
+	function new(element: EitherType<Element, String>);
+
+	/** Gets the tab instance associated with the specified DOM element. **/
+	static function getInstance(element: Element): Tab;
+
+	/** Destroys an element's tab. **/
+	function dispose(): Void;
+
+	/** Selects the given tab and shows its associated pane. **/
+	function show(): Void;
+}

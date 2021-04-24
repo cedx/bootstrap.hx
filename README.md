7fdbfae3b181806fe32a4f46135c83eeda185c97
Cédric Belin <cedric@belin.io>
2021-04-24 18:15:55 +0200

    Updated the documentation

---
+++ README.md
@@ -0,0 +1,18 @@
+# Bootstrap Bundle for Haxe
+![Bootstrap](https://badgen.net/badge/bootstrap/v5.0.0-beta3/yellow) ![Bootstrap Icons](https://badgen.net/badge/bs-icons/v1.4.1/yellow)  
+![Haxe](https://badgen.net/badge/haxe/%3E%3D4.2.0/green) ![Haxelib](https://badgen.net/haxelib/v/bootstrap_bundle) ![Downloads](https://badgen.net/haxelib/d/bootstrap_bundle) ![License](https://badgen.net/badge/license/MIT/blue)
+
+> Bundle providing [Bootstrap](https://getbootstrap.com) and [Bootstrap Icons](https://icons.getbootstrap.com)
+> in one [Haxe](https://haxe.org) package.
+
+## Documentation
+- [User guide](https://cedx.github.io/bootstrap.hx)
+- [API reference](https://cedx.github.io/bootstrap.hx/api)
+
+## Development
+- [Git repository](https://github.com/cedx/bootstrap.hx)
+- [Haxelib package](https://lib.haxe.org/p/bootstrap_bundle)
+- [Submit an issue](https://github.com/cedx/bootstrap.hx/issues)
+
+## License
+[Bootstrap Bundle for Haxe](https://cedx.github.io/bootstrap.hx) is distributed under the MIT License.
--- tool/doc.ps1
+++ tool/doc.ps1
@@ -5,21 +5,17 @@ Set-Location (Split-Path $PSScriptRoot)
 if (Test-Path "docs/api") { Remove-Item "docs/api" -Force -Recurse }
 
 $version = (Get-Content haxelib.json | ConvertFrom-Json).version
-haxe --define doc-gen --xml var/api.xml "build_$item.hxml"
+haxe --define doc-gen --no-output --xml var/api.xml build.hxml
 
-$target = $item.ToUpper()
 lix run dox `
-	--define description "Haxe type definitions for common $target libraries." `
+	--define description "Bundle providing Bootstrap and Bootstrap Icons in one Haxe package." `
 	--define source-path "https://github.com/cedx/bootstrap.hx/blob/main/src" `
 	--define themeColor 0x165898 `
 	--define version $version `
 	--define website "https://cedx.github.io/bootstrap.hx" `
-	--include "^js\.browser\." `
-	--include "^js\.npm\." `
-	--include "^php\.composer\." `
 	--input-path var `
-	--output-path "docs/api" `
-	--title "Haxe externs for $target" `
+	--output-path docs/api `
+	--title "Bootstrap Bundle for Haxe" `
 	--toplevel-package $item
 
-Copy-Item docs/favicon.ico "docs/api"
+Copy-Item docs/favicon.ico docs/api

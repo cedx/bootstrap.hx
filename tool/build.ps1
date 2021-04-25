5843a63aa05c83fc626e7ce0f951fd74b5146c37
Cédric Belin <cedric@belin.io>
2021-04-25 18:44:57 +0200

    Updated the build system

--- tool/build.ps1
+++ tool/build.ps1
@@ -3,14 +3,12 @@ Set-StrictMode -Version Latest
 Set-Location (Split-Path $PSScriptRoot)
 
 if (Test-Path "lib") { Remove-Item "lib" -Force -Recurse }
-foreach ($item in "css", "fonts", "icons", "js", "scss") { New-Item -ItemType Directory "lib/$item" }
 
-New-Item -ItemType Directory lib/css
-Copy-Item -Recurse node_modules/bootstrap/dist/css/*.css lib/css
+Copy-Item -Recurse node_modules/bootstrap/dist/css lib/css
+Copy-Item -Recurse node_modules/bootstrap/dist/js lib/js
+Copy-Item -Recurse node_modules/bootstrap/scss lib/scss
+Copy-Item -Recurse node_modules/bootstrap-icons/icons lib/icons
+Copy-Item -Recurse node_modules/bootstrap-icons/font/fonts lib/fonts
 
-New-Item -ItemType Directory lib/js
-Copy-Item -Recurse node_modules/bootstrap/dist/js/*.js lib/js
-
-Copy-Item -Recurse node_modules/bootstrap-icons/icons lib
-Copy-Item -Recurse node_modules/bootstrap-icons/font/fonts lib
-Copy-Item -Recurse node_modules/bootstrap/scss lib
+Copy-Item node_modules/bootstrap-icons/font/bootstrap-icons.css lib/css
+(Get-Content lib/css/bootstrap-icons.css) -replace "./fonts/", "../fonts/" | Out-File lib/css/bootstrap-icons.css

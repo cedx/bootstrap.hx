651a1951172048b9bbf5e941691c3dfeb1de8327
Cédric Belin <cedric@belin.io>
2021-06-05 19:09:38 +0200

    Code formatting

--- src/bootstrap/cli/CopyCommand.hx
+++ src/bootstrap/cli/CopyCommand.hx
@@ -39,13 +39,13 @@ class CopyCommand {
 		if (rest.length < requiredArgs || (haxelibRun && rest.length < requiredArgs + 1))
 			return new Error(BadRequest, "You must provide the path of the output directory.");
 
-		final inputs = ["css", "fonts", "icons", "js", "scss"];
-		var directories = inputs.filter(source -> Reflect.field(this, source));
-		if (directories.length == 0) directories = inputs;
+		final sources = ["css", "fonts", "icons", "js", "scss"];
+		var directories = sources.filter(source -> Reflect.field(this, source));
+		if (directories.length == 0) directories = sources;
 
-		final libDir = Path.join([Sys.getCwd(), "lib"]);
+		final input = Path.join([Sys.getCwd(), "lib"]);
 		final output = rest[0].isAbsolute() ? rest[0] : Path.join([haxelibRun ? rest[rest.length - 1] : Sys.getCwd(), rest[0]]);
-		for (directory in directories) copyDirectory(Path.join([libDir, directory]), directories.length == 1 ? output : Path.join([output, directory]));
+		for (directory in directories) copyDirectory(Path.join([input, directory]), directories.length == 1 ? output : Path.join([output, directory]));
 		return Noise;
 	}
 

676990cf30da8706ee60e945e3bdcf7723a67604
Cédric Belin <cedric@belin.io>
2021-06-06 23:36:35 +0200

    Code factorization

--- src/coconut/bootstrap/CloseButton.hx
+++ src/coconut/bootstrap/CloseButton.hx
@@ -6,16 +6,16 @@ class CloseButton {
 	/** Value indicating whether this button is disabled. **/
 	@:attribute var disabled: Bool = false;
 
-	/** Value indicating whether this button is white. **/
-	@:attribute var isWhite: Bool = false;
-
 	/** The button label. **/
 	@:attribute var label: String = "Close";
+
+	/** Value indicating whether this button is white. **/
+	@:attribute var white: Bool = false;
 
 	/** The handler of "click" events. **/
 	@:attribute function onClick(): Void;
 
 	/** Renders this view. **/
 	function render()
-		<button aria-label=${label} class=${isWhite ? ["btn-close", "btn-close-white"] : ["btn-close"]} disabled=${disabled} type="button"></button>;
+		<button aria-label=${label} class=${{"btn-close": true, "btn-close-white": white}} disabled=${disabled} type="button"></button>;
 }

83a6a99f9185346efd06fbab239225f0da984d0c
Cédric Belin <cedric@belin.io>
2021-06-08 22:51:40 +0200

    Added externs for button component

---
+++ src/bootstrap/Button.hx
@@ -0,0 +1,18 @@
+package bootstrap;
+
+import haxe.extern.EitherType;
+import js.html.Element;
+
+/** Represents a button. **/
+@:native("bootstrap.Button")
+extern class Button extends BaseComponent {
+
+	/** Creates a new button. **/
+	function new(element: EitherType<Element, String>);
+
+	/** Gets the button instance associated with the specified DOM element. **/
+	static function getInstance(element: Element): Button;
+
+	/** Toggles the push state. **/
+	function toggle(): Void;
+}

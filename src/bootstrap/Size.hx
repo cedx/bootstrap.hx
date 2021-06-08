1139542ad7cdb7a98bd90d9f30356af4dbfc636c
Cédric Belin <cedric@belin.io>
2021-06-08 22:51:06 +0200

    Added the `Size` enumeration

---
+++ src/bootstrap/Size.hx
@@ -0,0 +1,14 @@
+package bootstrap;
+
+/** Defines the size of a component. **/
+enum abstract Size(String) {
+
+	/** A default size. **/
+	var Default = "";
+
+	/** A large size. **/
+	var Large = "lg";
+
+	/** A small size. **/
+	var Small = "sm";
+}
---
+++ src/coconut/bootstrap/Size.hx
@@ -0,0 +1,4 @@
+package coconut.bootstrap;
+
+/** Defines the size of a component. **/
+typedef Size = bootstrap.Size;

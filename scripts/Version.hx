01ee8c90222682f628f7854d0d6f5c472f226c54
Cédric Belin <cedric@belin.io>
2021-06-11 08:38:30 +0200

    Updated the "version" task

--- scripts/Version.hx
+++ scripts/Version.hx
@@ -1,4 +1,6 @@
+//! --class-path src
 import Tools.updateFile;
+import bootstrap.Version;
 import haxe.DynamicAccess;
 import haxe.Json;
 import sys.io.File.*;
@@ -11,4 +13,10 @@ function main() {
 	final dependencies: DynamicAccess<String> = Json.parse(getContent("package.json")).dependencies;
 	updateFile("src/bootstrap/Version.hx", ~/bootstrap = "\d+(\.\d+){2}"/, 'bootstrap = "${dependencies["bootstrap"].substring(1)}"');
 	updateFile("src/bootstrap/Version.hx", ~/bootstrapIcons = "\d+(\.\d+){2}"/, 'bootstrapIcons = "${dependencies["bootstrap-icons"].substring(1)}"');
+
+	for (key => value in ["bootstrap" => Version.bootstrap, "bootstrap-icons" => Version.bootstrapIcons]) {
+		for (file in ["README.md", "docs/README.md"]) {
+			// TODO: updateFile("README.md", ~/bootstrap = "\d+(\.\d+){2}"/, 'bootstrap = "${dependencies["bootstrap"].substring(1)}"');
+		}
+	}
 }

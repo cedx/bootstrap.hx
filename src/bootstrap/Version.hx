d26241263902ccee45639e939d1cac9b7b11c1be
Cédric Belin <cedric@belin.io>
2021-06-14 12:40:23 +0200

    Added the `Version` class

---
+++ src/bootstrap/Version.hx
@@ -0,0 +1,11 @@
+package bootstrap;
+
+/** Provides information about the Bootstrap versions. **/
+abstract class Version {
+
+	/** The version number of the [Bootstrap](https://getbootstrap.com) library. **/
+	public static inline final bootstrap = "6.6.6";
+
+	/** The version number of the [Bootstrap Icons](https://icons.getbootstrap.com) library. **/
+	public static inline final bootstrapIcons = "6.6.6";
+}

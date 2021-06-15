8fa6da410febfe00fc26fd828b770f51c63e7645
Cédric Belin <cedric@belin.io>
2021-06-15 10:51:41 +0200

    Updated the "version" task

--- scripts/Version.hx
+++ scripts/Version.hx
@@ -12,12 +12,12 @@ function main() {
 
 	final dependencies: DynamicAccess<String> = Json.parse(getContent("package.json")).dependencies;
 	final versions = [
-		"bootstrap" => {pack: "bootstrap", version: dependencies["bootstrap"].substring(1)},
-		"bootstrapIcons" => {pack: "bootstrap-icons", version: dependencies["bootstrap-icons"].substring(1)}
+		{field: "bootstrap", pack: "bootstrap", version: dependencies["bootstrap"].substring(1)},
+		{field: "bootstrapIcons", pack: "bootstrap-icons", version: dependencies["bootstrap-icons"].substring(1)}
 	];
 
-	for (field => lib in versions)
-		updateFile("src/bootstrap/Version.hx", new EReg('$field = "\\d+(\\.\\d+){2}"', ""), '$field = "${lib.version}"');
+	for (lib in versions)
+		updateFile("src/bootstrap/Version.hx", new EReg('${lib.field} = "\\d+(\\.\\d+){2}"', ""), '${lib.field} = "${lib.version}"');
 
 	for (lib in versions) for (file in ["README.md", "docs/README.md"])
 		updateFile(file, new EReg('${lib.pack}/v\\d+(\\.\\d+){2}', ""), '${lib.pack}/v${lib.version}');

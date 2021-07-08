cb02306a1f08a41886db8ec030f7bf1f5b92d1d0
Cédric Belin <cedric@belin.io>
2021-07-08 17:47:06 +0200

    Code optimization

--- src/bootstrap/cli/CopyCommand.hx
+++ src/bootstrap/cli/CopyCommand.hx
@@ -39,18 +39,20 @@ class CopyCommand {
 		if (rest.length < requiredArgs || (haxelibRun && rest.length < requiredArgs + 1))
 			return new Error(BadRequest, "You must provide the path of the output directory.");
 
+		final output = rest[0].isAbsolute() ? rest[0] : Path.join([haxelibRun ? rest[rest.length - 1] : Sys.getCwd(), rest[0]]);
+		FileSystem.createDirectory(output);
+
 		final sources = ["css", "fonts", "icons", "js", "scss"];
 		var directories = sources.filter(source -> Reflect.field(this, source));
 		if (directories.length == 0) directories = sources;
 
 		final input = Path.join([Sys.getCwd(), "lib"]);
-		final output = rest[0].isAbsolute() ? rest[0] : Path.join([haxelibRun ? rest[rest.length - 1] : Sys.getCwd(), rest[0]]);
 		for (directory in directories) copyDirectory(Path.join([input, directory]), directories.length == 1 ? output : Path.join([output, directory]));
 		return Noise;
 	}
 
 	/** Recursively copies all files in the specified `source` directory to a given `destination` directory. **/
-	function copyDirectory(source: String, destination: String) for (entry in FileSystem.readDirectory(source)) {
+	static function copyDirectory(source: String, destination: String) for (entry in FileSystem.readDirectory(source)) {
 		final input = Path.join([source, entry]);
 		final output = Path.join([destination, entry]);
 		if (FileSystem.isDirectory(input)) copyDirectory(input, output);

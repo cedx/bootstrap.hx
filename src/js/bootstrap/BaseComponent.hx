2b768ae5f2cb8fef4f0d4584be25098e21515219
Cédric Belin <cedric@belin.io>
2021-09-02 12:26:59 +0200

    Added the `BaseComponent.VERSION` static property

--- src/js/bootstrap/BaseComponent.hx
+++ src/js/bootstrap/BaseComponent.hx
@@ -6,8 +6,17 @@ import js.html.Element;
 /** The base class for components. **/
 extern abstract class BaseComponent {
 
+	/** The version number of the [Bootstrap](https://getbootstrap.com) library. **/
+	static final VERSION: String;
+
 	/** Creates a new component. **/
 	function new(element: EitherType<Element, String>);
+
+	/** Gets the component instance associated with the specified DOM element or creates a new one in case it wasn't initialised. **/
+	static function getOrCreateInstance(element: EitherType<Element, String>): BaseComponent;
+
+	/** Gets the component instance associated with the specified DOM element. **/
+	static function getInstance(element: EitherType<Element, String>): BaseComponent;
 
 	/** Destroys this component. **/
 	function dispose(): Void;

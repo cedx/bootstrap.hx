02b6629f19b3b8be4494cb5c232d6930803af60d
Cédric Belin <cedric@belin.io>
2021-10-31 14:25:00 +0100

    Updated the code examples

--- example/alert.hx
+++ example/alert.hx
@@ -1,24 +1,27 @@
 import js.Browser.document;
 import js.bootstrap.Alert;
+import js.bootstrap.Variant;
 
 /** Creates an alert and a button to handle its closure. **/
 function main() {
 	document.body.innerHTML = render();
 
-	final alert = Alert.getOrCreateInstance(".alert");
+	final alert = document.querySelector(".alert");
+	alert.addEventListener(AlertEvent.Closed, () -> trace("The alert has been closed."));
+
 	final button = document.querySelector(".btn");
-	button.addEventListener("click", () -> alert.close());
+	button.addEventListener("click", () -> Alert.getOrCreateInstance(alert).close());
 }
 
 /** Returns the HTML content of the document body. **/
 private function render() return '
 	<div class="container">
-		<div class="alert alert-primary" role="alert">
+		<div class="alert alert-${Variant.Primary}" role="alert">
 			A simple primary alert - check it out!
 		</div>
 
 		<p>
-			<button class="btn btn-primary" type="button">
+			<button class="btn btn-${Variant.Primary}" type="button">
 				Close alert
 			</button>
 		</p>

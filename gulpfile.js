4059d132e252ee9cc08426b86f35d81dbef31320
Cédric Belin <cedric@belin.io>
2022-05-01 01:38:46 +0200

    Updated the build system [skip ci]

--- gulpfile.js
+++ gulpfile.js
@@ -1,22 +1,14 @@
-import {spawn} from "node:child_process";
 import {createWriteStream, existsSync, readFileSync} from "node:fs";
 import {cp} from "node:fs/promises";
 import archiver from "archiver";
 import del from "del";
+import {execa} from "execa";
 import gulp from "gulp";
 import replace from "gulp-replace";
 import merge from "merge2";
 
 // The package configuration.
 const pkg = JSON.parse(readFileSync("haxelib.json", "utf8"));
-
-/** The default task. */
-export default gulp.series(
-	clean,
-	build,
-	buildIcons,
-	version
-);
 
 /** Builds the project. */
 export async function build() {
@@ -54,14 +46,14 @@ export async function doc() {
 	await exec("haxe", ["--define", "doc-gen", "--no-output", "--xml", "var/api.xml", "build.hxml"]);
 	await exec("lix", [
 		"run", "dox",
-		"--define", "description", '"Bundle providing Bootstrap and Bootstrap Icons in one Haxe package."',
+		"--define", "description", "Bundle providing Bootstrap and Bootstrap Icons in one Haxe package.",
 		"--define", "source-path", "https://github.com/cedx/bootstrap.hx/blob/main/src",
 		"--define", "themeColor", "0xffc105",
 		"--define", "version", pkg.version,
 		"--define", "website", "https://github.com/cedx/bootstrap.hx",
 		"--input-path", "var",
 		"--output-path", "docs",
-		"--title", '"Bootstrap Bundle for Haxe"',
+		"--title", "Bootstrap Bundle for Haxe",
 		"--toplevel-package", "js.bootstrap"
 	]);
 
@@ -76,8 +68,8 @@ export async function install() {
 
 /** Performs the static analysis of source code. */
 export async function lint() {
-	await exec("lix", ["run", "checkstyle", "--config", "etc/checkstyle.json", "--source src"]);
-	return exec("npx", ["tsc", "--project", "jsconfig.json"]);
+	await exec("lix", ["run", "checkstyle", "--config", "etc/checkstyle.json", "--exitcode", "--source", "src"]);
+	return exec("tsc", ["--project", "jsconfig.json"]);
 }
 
 /** Publishes the package in the registry. */
@@ -103,13 +95,21 @@ export function version() {
 		.pipe(gulp.dest("."));
 }
 
+/** The default task. */
+export default gulp.series(
+	clean,
+	build,
+	buildIcons,
+	version
+);
+
 /**
- * Spawns a new process using the specified command.
+ * Runs the specified command.
  * @param {string} command The command to run.
  * @param {string[]} [args] The command arguments.
- * @returns {Promise<void>} Resolves when the command is finally terminated.
+ * @param {import("execa").Options} [options] The child process options.
+ * @returns {import("execa").ExecaChildProcess} Resolves when the command is finally terminated.
  */
-function exec(command, args = []) {
-	return new Promise((resolve, reject) => spawn(command, args, {shell: true, stdio: "inherit"})
-		.on("close", code => code ? reject(new Error(args.length ? `${command} ${args.join(" ")}` : command)) : resolve()));
+function exec(command, args = [], options = {}) {
+	return execa(command, args, {preferLocal: true, stdio: "inherit", ...options});
 }

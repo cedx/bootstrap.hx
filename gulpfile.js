6003438be7e1de4af045889a1b96d2cbd2513aa7
Cédric Belin <cedric@belin.io>
2022-06-28 15:12:54 +0200

    Using import assertions

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,4 @@
-import {createWriteStream, existsSync, readFileSync} from "node:fs";
+import {createWriteStream, existsSync} from "node:fs";
 import {cp, rm} from "node:fs/promises";
 import archiver from "archiver";
 import del from "del";
@@ -7,9 +7,8 @@ import log from "fancy-log";
 import gulp from "gulp";
 import replace from "gulp-replace";
 import merge from "merge2";
-
-// The package configuration.
-const pkg = JSON.parse(readFileSync("haxelib.json", "utf8"));
+import haxelib from "./haxelib.json" assert {type: "json"};
+import pkg from "./package.json" assert {type: "json"};
 
 /** Builds the project. */
 export const build = gulp.series(
@@ -47,7 +46,7 @@ export async function doc() {
 		"--define", "description", "Bundle providing Bootstrap and Bootstrap Icons in one Haxe package.",
 		"--define", "source-path", "https://github.com/cedx/bootstrap.hx/blob/main/src",
 		"--define", "themeColor", "0xffc105",
-		"--define", "version", pkg.version,
+		"--define", "version", haxelib.version,
 		"--define", "website", "https://github.com/cedx/bootstrap.hx",
 		"--input-path", "var",
 		"--output-path", "docs",
@@ -79,17 +78,16 @@ export async function publish() {
 	archive.on("error", error => { throw error; }).pipe(output);
 	archive.glob("*.md").glob("haxelib.json").glob("run.n").directory("lib", "lib").directory("src", "src").finalize();
 
-	for (const command of [["tag"], ["push", "origin"]]) await exec("git", [...command, `v${pkg.version}`]);
+	for (const command of [["tag"], ["push", "origin"]]) await exec("git", [...command, `v${haxelib.version}`]);
 	log("The package is ready to be published using 'haxelib submit var/haxelib.zip'.");
 }
 
 /** Updates the version numbers in the sources. */
 export function version() {
-	const {dependencies} = JSON.parse(readFileSync("package.json", "utf8"));
 	return gulp.src(["README.md", "package.json"])
-		.pipe(replace(/bootstrap\/v\d+(\.\d+){2}/, `bootstrap/v${dependencies.bootstrap.substring(1)}`))
-		.pipe(replace(/bootstrap-icons\/v\d+(\.\d+){2}/, `bootstrap-icons/v${dependencies["bootstrap-icons"].substring(1)}`))
-		.pipe(replace(/"version": "\d+(\.\d+){2}"/, `"version": "${pkg.version}"`))
+		.pipe(replace(/bootstrap\/v\d+(\.\d+){2}/, `bootstrap/v${pkg.dependencies.bootstrap.substring(1)}`))
+		.pipe(replace(/bootstrap-icons\/v\d+(\.\d+){2}/, `bootstrap-icons/v${pkg.dependencies["bootstrap-icons"].substring(1)}`))
+		.pipe(replace(/"version": "\d+(\.\d+){2}"/, `"version": "${haxelib.version}"`))
 		.pipe(gulp.dest("."));
 }
 

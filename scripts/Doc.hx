7be9043005ae3f892c35f496c6d104bf6ec2208e
Cédric Belin <cedric@belin.io>
2023-05-25 23:28:41 +0200

    Update the "Doc" task

--- scripts/Doc.hx
+++ scripts/Doc.hx
@@ -13,7 +13,7 @@ function main() {
 		"--define", "source-path", "https://github.com/cedx/bootstrap.hx/blob/main/src",
 		"--define", "themeColor", "0xea8220",
 		"--define", "version", Version.packageVersion,
-		"--define", "website", "https://github.com/cedx/bootstrap.hx",
+		"--define", "website", "https://docs.belin.io/bootstrap.hx",
 		"--input-path", "var",
 		"--output-path", "docs",
 		"--title", "Bootstrap Bundle for Haxe",

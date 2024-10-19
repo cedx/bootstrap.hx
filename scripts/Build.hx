0fafb2ad7b41bd2108cd95ef07d28a341e30d6cb
Cédric Belin <cedric@belin.io>
2024-10-19 13:23:47 +0200

    Update the build system

--- scripts/Build.hx
+++ scripts/Build.hx
@@ -5,7 +5,7 @@ using haxe.io.Path;
 /** Builds the project. **/
 function main() {
 	Sys.command("haxe run.hxml");
-	if (FileSystem.exists("lib")) Tools.removeDirectory("lib");
+	if (FileSystem.exists("lib")) removeDirectory("lib");
 
 	final sources = [
 		"bootstrap/dist/css",
@@ -15,13 +15,40 @@ function main() {
 		"bootstrap-icons/font/fonts"
 	];
 
-	for (source in sources) Tools.copyDirectory('node_modules/$source', 'lib/${source.withoutDirectory()}', ~/\.map$/i);
+	for (source in sources) copyDirectory('node_modules/$source', 'lib/${source.withoutDirectory()}', ~/\.map$/i);
 	for (directory in ["css", "js"])
-		for (file in FileSystem.readDirectory('lib/$directory')) Tools.replaceInFile('lib/$directory/$file', ~/\s*(\/\/|\/\*)# sourceMappingURL=.*$/s, "");
+		for (file in FileSystem.readDirectory('lib/$directory')) replaceInFile('lib/$directory/$file', ~/\s*(\/\/|\/\*)# sourceMappingURL=.*$/s, "");
 
 	for (extension in ["css", "scss"]) {
 		final output = 'lib/$extension/bootstrap-icons.$extension';
 		File.copy('node_modules/bootstrap-icons/font/bootstrap-icons.$extension', output);
-		Tools.replaceInFile(output, ~/\.\/fonts/g, "../fonts");
+		replaceInFile(output, ~/\.\/fonts/g, "../fonts");
 	}
 }
+
+/** Recursively deletes all files in the specified `directory`. **/
+private function cleanDirectory(directory: String) for (entry in FileSystem.readDirectory(directory).filter(entry -> entry != ".gitkeep")) {
+	final path = Path.join([directory, entry]);
+	FileSystem.isDirectory(path) ? removeDirectory(path) : FileSystem.deleteFile(path);
+}
+
+/** Recursively copies all files in the specified `source` directory to a given `destination` directory. **/
+function copyDirectory(source: String, destination: String, ?exclude: EReg) for (entry in FileSystem.readDirectory(source)) {
+	final input = Path.join([source, entry]);
+	final output = Path.join([destination, entry]);
+	if (FileSystem.isDirectory(input)) copyDirectory(input, output, exclude);
+	else if (exclude == null || !exclude.match(input.withoutDirectory())) {
+		FileSystem.createDirectory(output.directory());
+		File.copy(input, output);
+	}
+}
+
+/** Recursively deletes the specified `directory`. **/
+private function removeDirectory(directory: String) {
+	cleanDirectory(directory);
+	FileSystem.deleteDirectory(directory);
+}
+
+/** Replaces in the specified `file` the substring which the `pattern` matches with the given `replacement`. **/
+private function replaceInFile(file: String, pattern: EReg, replacement: String)
+	File.saveContent(file, pattern.replace(File.getContent(file), replacement));
--- scripts/Version.hx
+++ scripts/Version.hx
@@ -6,11 +6,15 @@ import sys.io.File;
 
 /** Updates the version number in the sources. **/
 function main() {
-	Tools.replaceInFile("package.json", ~/"version": "\d+(\.\d+){2}"/, '"version": "${Platform.packageVersion}"');
+	replaceInFile("package.json", ~/"version": "\d+(\.\d+){2}"/, '"version": "${Platform.packageVersion}"');
 
 	final dependencies: DynamicAccess<String> = Json.parse(File.getContent("package.json")).dependencies;
 	for (library in ["bootstrap", "bootstrap-icons"]) {
 		final version = dependencies[library].substr(1);
-		Tools.replaceInFile("README.md", new EReg('$library/v\\d+(\\.\\d+){2}', ""), '$library/v$version');
+		replaceInFile("README.md", new EReg('$library/v\\d+(\\.\\d+){2}', ""), '$library/v$version');
 	}
 }
+
+/** Replaces in the specified `file` the substring which the `pattern` matches with the given `replacement`. **/
+private function replaceInFile(file: String, pattern: EReg, replacement: String)
+	File.saveContent(file, pattern.replace(File.getContent(file), replacement));

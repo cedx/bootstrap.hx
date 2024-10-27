56453e343308868defb43c09edfe9c9a5764cc04
Cédric Belin <cedric@belin.io>
2024-10-27 11:35:32 +0100

    Rename and move the `BootstrapImporter` class to `SassImporter`

---
+++ src/bootstrap/SassImporter.hx
@@ -0,0 +1,24 @@
+package bootstrap;
+
+import js.node.ChildProcess;
+import js.node.Url;
+import js.node.url.URL;
+using StringTools;
+using haxe.io.Path;
+
+/** Loads `bootstrap:` URLs according to the location of the `bootstrap_bundle` package. **/
+class SassImporter {
+
+	/** The file URL of the `bootstrap_bundle` package. **/
+	final pkgUrl: URL;
+
+	/** Creates a new Bootstrap importer. **/
+	public function new(?entryPointDirectory: String) {
+		entryPointDirectory ??= ChildProcess.execFileSync("lix", ["run", "bootstrap_bundle", "libpath"], {encoding: "utf8", shell: true}).rtrim();
+		pkgUrl = Url.pathToFileURL(Path.join([entryPointDirectory, "scss"]).addTrailingSlash());
+	}
+
+	/** Partially resolves a load (such as `@use` or `@import`) to a file on disk. **/
+	public function findFileUrl(url: String): Null<URL>
+		return url.startsWith("bootstrap:") ? new URL(url.substr(10), pkgUrl) : null;
+}
